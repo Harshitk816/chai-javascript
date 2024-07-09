ed32bb1f4aa97f883262f37b36e20f5ef59b8455
Harshitk816 <harshitkaushal816@gmail.com>
2024-07-09 17:52:30 +0530

    Add variables and datatypes examples

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,6 @@
+const accountId = 2010991265
+let accountEmail = "harshit1265.be20@chitkara.edu.in"
+var accountPassword = "12345"
+accountCity = "Rajpura"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+let state
+
+// number => 2 to pow 54
+//string => ""
+//bigint 
+//boolean => true/false
+//null => standalone value
+//undefined
+//symbol => unique
+
+//object
+
+console.log(typeof undefined)//undefined
+console.log(typeof null)//object
