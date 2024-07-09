5a5f3be8c4c0835220df83092cb2dd9677e5133d
Harshitk816 <harshitkaushal816@gmail.com>
2024-07-09 18:24:56 +0530

    Add conversion operations example

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,22 @@
+let score = "54"
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInScore = Number(score)
+//console.log(typeof(valueInScore))
+
+//"33"=> 33
+//"33abc"=>NaN
+//true => 1; false=>0
+
+let isLoggedIn = 1
+
+console.log(Boolean(isLoggedIn))
+
+let someNumber = 33
+console.log(String(typeof(someNumber)))
+console.log(typeof(String(someNumber)))
+
+
+
