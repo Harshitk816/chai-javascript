596dfc276f8359a0e45a243e184cf8d62057846e
Harshitk816 <harshitkaushal816@gmail.com>
2024-07-27 17:57:24 +0530

    Add code for number manipulation and math operations

---
+++ 01_basics/07_nums_and_maths.js
@@ -0,0 +1,26 @@
+const bal = 400
+const score = new Number(100)
+
+// console.log(bal)
+// console.log(score);
+// console.log(score.toString());
+// console.log(score.toFixed(2));
+
+const money = 4500660.54
+
+// console.log(money.toLocaleString('en-IN'));
+
+//++++++++++++++++++ MATHS +++++++++++++++++++//
+// console.log(Math.abs(-32));
+// console.log(Math.round(34.24));
+// console.log(Math.ceil(4.3));
+// console.log(Math.floor(4.6));
+console.log(Math.round((Math.random()*10) + 1));
+
+
+
+
+
+
+
+
