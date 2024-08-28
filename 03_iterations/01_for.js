f8e3af7c3c3eedf7e82e5b97cab3f82c978a8b3f
Harshitk816 <harshitkaushal816@gmail.com>
2024-08-29 02:44:54 +0530

    basic iterators done

---
+++ 03_iterations/01_for.js
@@ -0,0 +1,21 @@
+//for loop
+
+
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    console.log(element);
+    
+    
+}
+
+//for loops for array
+ let myArr = ['apple', 'mango', 'grapes']
+
+ for (let i = 0; i < myArr.length; i++) {
+    const element = myArr[i];
+    console.log(element);
+    
+    
+ }
+
+ //break-continue
---
+++ 03_iterations/02_while.js
@@ -0,0 +1,23 @@
+//while loop
+
+let index = 0;
+while (index <= 10) {
+//   console.log(`Index value is ${index}`);
+  index = index+2;
+}
+
+//for arrays
+
+let myArr = ['apple', 'mango', 'grapes']
+let i = 0;
+while (i < myArr.length) {
+//   console.log(myArr[i]);
+  i++;
+}
+
+//do-while loop
+let score = 11
+do {
+console.log(`Score is ${score}`);
+score++ 
+} while (score<=10);
